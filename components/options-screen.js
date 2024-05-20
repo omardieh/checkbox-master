b16b9a2b0e212cc9d2d6f255e015db38bdafa4ce
omardieh <omar.dieh@wildcodeschool.com>
2024-05-20 19:07:09 +0200

    all tested, working checkpoint

--- components/options-screen.js
+++ components/options-screen.js
@@ -1,36 +1,85 @@
 export default class OptionsScreen {
-  constructor(root, checkButton, uncheckButton, counter) {
+  constructor(root) {
     this.root = root;
-    this.checkButton = checkButton;
-    this.uncheckButton = uncheckButton;
-    this.counter = counter;
+    this.checkButton = this.createElement({
+      type: "button",
+      textContent: "Check All",
+    });
+    this.uncheckButton = this.createElement({
+      type: "button",
+      textContent: "Uncheck All",
+    });
+    this.counter = this.createElement({
+      type: "span",
+      textContent: "Selected 0",
+    });
+    this.updateCountText = this.updateCountText.bind(this);
     this.render();
   }
 
   render() {
-    const optionsScreen = document.createElement("div");
-    optionsScreen.classList.add("options-screen");
+    const optionsScreen = this.createElement({
+      type: "div",
+      className: "options-screen",
+    });
 
-    const headerTitle = document.createElement("h2");
-    headerTitle.innerText = document.title;
-    optionsScreen.appendChild(headerTitle);
+    const headerTitle = this.createElement({
+      type: "h2",
+      textContent: document.title,
+    });
+    const featuresText = [
+      "Check All / Uncheck All Buttons",
+      "Real-Time Count Updates",
+      "Easy-to-Use Interface",
+      "Supports Iframe HTML",
+    ];
+    const featuresItems = featuresText.map((text) =>
+      this.createElement({ type: "li", textContent: text })
+    );
+    const featuresList = this.createElement({
+      type: "ul",
+      className: "features-list",
+    });
+    featuresList.append(...featuresItems);
+    const buttonContainer = this.createElement({
+      type: "div",
+      className: "button-container",
+    });
+    buttonContainer.append(this.checkButton, this.uncheckButton);
 
-    const buttonContainer = document.createElement("div");
-    buttonContainer.classList.add("button-container");
-    buttonContainer.appendChild(this.checkButton);
-    buttonContainer.appendChild(this.uncheckButton);
-    optionsScreen.appendChild(buttonContainer);
-
-    const counterContainer = document.createElement("div");
-    counterContainer.classList.add("counter-container");
+    const counterContainer = this.createElement({
+      type: "div",
+      className: "counter-container",
+    });
     counterContainer.appendChild(this.counter);
-    optionsScreen.appendChild(counterContainer);
 
-    const footerTitle = document.createElement("p");
-    footerTitle.innerText = "Powered by Omar Dieh";
-    footerTitle.classList.add("footer");
-    optionsScreen.appendChild(footerTitle);
+    const footerTitle = this.createElement({
+      type: "p",
+      className: "footer",
+      innerHTML: `Powered by <a href="https://github.com/omardieh" target="_blank"> Omar Dieh </a>`,
+    });
+
+    optionsScreen.append(
+      headerTitle,
+      featuresList,
+      buttonContainer,
+      counterContainer,
+      footerTitle
+    );
 
     this.root.appendChild(optionsScreen);
+  }
+
+  createElement({ type, className, textContent, innerHTML, styles }) {
+    const element = document.createElement(type);
+    if (className) element.className = className;
+    if (textContent) element.textContent = textContent;
+    if (innerHTML) element.innerHTML = innerHTML;
+    if (styles) Object.assign(element.style, styles);
+    return element;
+  }
+
+  updateCountText(count) {
+    this.counter.textContent = "Selected " + count;
   }
 }
